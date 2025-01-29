e59df197e7997cf3fba90c2af1b7d3f1a4f17de8
dylanR33 <ruiz.dylaneduardo@gmail.com>
2025-01-29 00:00:35 -0800

    Did basic configuration of ADC peripheral.

---
+++ src/AdcInit/AdcResourcesInit.c
@@ -0,0 +1,65 @@
+#include "stm32f4xx_hal.h"
+#include "stm32f4xx_hal_adc.h"
+#include "stm32f4xx_hal_gpio.h"
+#include "stm32f4xx_hal_gpio_ex.h"
+
+// ADC Pins
+//  currentPhaseA: PC0     ADC123_IN10 ADC1
+//  currentPhaseB: PC1     ADC123_IN11 ADC2
+//  currectPhaseC: PC2     ADC123_IN12 ADC3
+
+void HAL_ADC_MspInit( ADC_HandleTypeDef* hadc )
+{
+    if ( hadc->Instance == ADC1 )
+    {
+        // Enable adc clock __HAL_RCC_ADC_CLK_ENABLE()
+        __HAL_RCC_ADC1_CLK_ENABLE();
+
+        // Enable gpio port clocks
+        __HAL_RCC_GPIOC_CLK_ENABLE();
+
+        // Configure pins to analog mode
+        GPIO_InitTypeDef phaseA;
+
+        phaseA.Pin  = GPIO_PIN_0;
+        phaseA.Mode = GPIO_MODE_ANALOG;
+        phaseA.Pull = GPIO_NOPULL;
+     
+        HAL_GPIO_Init( GPIOC, &phaseA );
+    }
+    else if ( hadc->Instance == ADC2 )
+    {
+        // Enable adc clock __HAL_RCC_ADC_CLK_ENABLE()
+        __HAL_RCC_ADC2_CLK_ENABLE();
+
+        // Enable gpio port clocks
+        __HAL_RCC_GPIOC_CLK_ENABLE();
+
+        // Configure pins to analog mode
+        GPIO_InitTypeDef phaseB;
+
+        phaseB.Pin  = GPIO_PIN_1;
+        phaseB.Mode = GPIO_MODE_ANALOG;
+        phaseB.Pull = GPIO_NOPULL;
+     
+        HAL_GPIO_Init( GPIOC, &phaseB );
+    }
+    else if ( hadc->Instance == ADC3 )
+    {
+        // Enable adc clock __HAL_RCC_ADC_CLK_ENABLE()
+        __HAL_RCC_ADC3_CLK_ENABLE();
+
+        // Pin config
+        // Enable gpio port clocks
+        __HAL_RCC_GPIOC_CLK_ENABLE();
+
+        // Configure pins to analog mode
+        GPIO_InitTypeDef phaseC;
+
+        phaseC.Pin  = GPIO_PIN_2;
+        phaseC.Mode = GPIO_MODE_ANALOG;
+        phaseC.Pull = GPIO_NOPULL;
+     
+        HAL_GPIO_Init( GPIOC, &phaseC );
+    }
+}
---
+++ src/AdcInit/PhaseCurrentAdc.c
@@ -0,0 +1,102 @@
+#include <stdint.h>
+
+#include "stm32f4xx_hal.h"
+#include "stm32f4xx_hal_adc.h"
+#include "stm32f4xx_hal_gpio.h"
+#include "stm32f4xx_hal_gpio_ex.h"
+
+static ADC_HandleTypeDef phaseA_hadc;
+static ADC_HandleTypeDef phaseB_hadc;
+static ADC_HandleTypeDef phaseC_hadc;
+
+void ADC_Config()
+{
+    // ------------- Phase A ADC (master) config ----------------------
+    phaseA_hadc.Instance = ADC1;
+    phaseA_hadc.Init.ClockPrescaler = ADC_CLOCK_SYNC_PCLK_DIV4;
+    phaseA_hadc.Init.Resolution = ADC_RESOLUTION_12B;
+    phaseA_hadc.Init.DataAlign = ADC_DATAALIGN_RIGHT;
+    phaseA_hadc.Init.EOCSelection = ADC_EOC_SINGLE_CONV;
+    phaseA_hadc.Init.ContinuousConvMode = DISABLE;
+    phaseA_hadc.Init.ExternalTrigConv = ADC_SOFTWARE_START;
+
+    HAL_ADC_Init( &phaseA_hadc );
+
+    ADC_MultiModeTypeDef multi =
+    {
+        .Mode = ADC_TRIPLEMODE_REGSIMULT,
+        .TwoSamplingDelay = ADC_TWOSAMPLINGDELAY_5CYCLES
+    };
+
+    HAL_ADCEx_MultiModeConfigChannel( &phaseA_hadc, &multi );
+
+    ADC_ChannelConfTypeDef phaseAchan = 
+    {
+        .Channel = ADC_CHANNEL_10,
+        .Rank = 1, // 0 - 16
+        .SamplingTime = ADC_SAMPLETIME_3CYCLES
+    };
+
+    HAL_ADC_ConfigChannel( &phaseA_hadc, &phaseAchan );
+
+    // ------------- Phase B ADC config ----------------------
+    phaseB_hadc.Instance = ADC2;
+    phaseB_hadc.Init.ClockPrescaler = ADC_CLOCK_SYNC_PCLK_DIV4;
+    phaseB_hadc.Init.Resolution = ADC_RESOLUTION_12B;
+    phaseB_hadc.Init.DataAlign = ADC_DATAALIGN_RIGHT;
+    phaseB_hadc.Init.EOCSelection = ADC_EOC_SINGLE_CONV;
+    phaseB_hadc.Init.ContinuousConvMode = DISABLE;
+    phaseB_hadc.Init.ExternalTrigConv = ADC_SOFTWARE_START;
+
+    HAL_ADC_Init( &phaseB_hadc );
+
+    ADC_ChannelConfTypeDef phaseBchan = 
+    {
+        .Channel = ADC_CHANNEL_11,
+        .Rank = 1, // 0 - 16
+        .SamplingTime = ADC_SAMPLETIME_3CYCLES
+    };
+
+    HAL_ADC_ConfigChannel( &phaseB_hadc, &phaseBchan );
+
+    // ------------- Phase C ADC config ----------------------
+    phaseC_hadc.Instance = ADC3;
+    phaseC_hadc.Init.ClockPrescaler = ADC_CLOCK_SYNC_PCLK_DIV4;
+    phaseC_hadc.Init.Resolution = ADC_RESOLUTION_12B;
+    phaseC_hadc.Init.DataAlign = ADC_DATAALIGN_RIGHT;
+    phaseC_hadc.Init.EOCSelection = ADC_EOC_SINGLE_CONV;
+    phaseC_hadc.Init.ContinuousConvMode = DISABLE;
+    phaseC_hadc.Init.ExternalTrigConv = ADC_SOFTWARE_START;
+
+    HAL_ADC_Init( &phaseC_hadc );
+
+    ADC_ChannelConfTypeDef phaseCchan = 
+    {
+        .Channel = ADC_CHANNEL_12,
+        .Rank = 1, // 0 - 16
+        .SamplingTime = ADC_SAMPLETIME_3CYCLES
+    };
+
+    HAL_ADC_ConfigChannel( &phaseC_hadc, &phaseCchan );
+
+    // Start ADC's
+    HAL_ADC_Start( &phaseA_hadc );
+    HAL_ADC_Start( &phaseB_hadc );
+    HAL_ADC_Start( &phaseC_hadc );
+}
+
+
+uint32_t PhaseCurrentAdc_GetPhaseA()
+{
+    return HAL_ADC_GetValue( &phaseA_hadc );
+}
+
+uint32_t PhaseCurrnetAdc_GetPhaseB()
+{
+    return HAL_ADC_GetValue( &phaseB_hadc );
+}
+
+uint32_t PhaseCurrentAdc_GetPhaseC()
+{
+    return HAL_ADC_GetValue( &phaseC_hadc );
+}

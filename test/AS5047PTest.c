552c170c9a2f5bc44e97b83378908a0111aec0d0
dylanR33 <ruiz.dylaneduardo@gmail.com>
2025-02-23 14:35:09 -0800

    Updated rotary encoder tests for new spi read and write functions.

--- test/AS5047PTest.c
+++ test/AS5047PTest.c
@@ -10,19 +10,25 @@
 TEST_GROUP( AS5047P );
 
 
+static uint8_t nextReading[ sizeof( uint32_t ) ] = { 0, 0 };
+
 TEST_SETUP( AS5047P )
 {
     AS5047PInterface interface =
     {
-        .spiWrite = WriteSpy_Write32,
-        .spiRead  = FakeRead_Read32,
+        .spiWrite = WriteSpy_Write8Arr,
+        .spiRead  = FakeRead_Read8Arr,
         .spiSetCS = WriteSpy_Write8
     };
 
     AS5047P_SetInterface( &interface );
     
     WriteSpy_Create();
+
     FakeRead_Create();
+    nextReading[ 0 ] = 0;
+    nextReading[ 1 ] = 0;
+    FakeRead_SetNextReading8Arr( nextReading, sizeof( nextReading ) );
 }
 
 TEST_TEAR_DOWN( AS5047P )
@@ -33,106 +39,129 @@ TEST( AS5047P, SetInterface_RequireBothInterfaces )
 {
     AS5047PInterface interface = 
     {
-        .spiWrite = WriteSpy_Write32,
+        .spiWrite = WriteSpy_Write8Arr,
         .spiRead = NULL
     };
 
     TEST_ASSERT_EQUAL( AS5047P_INTERFACE_UNSET, AS5047P_SetInterface( &interface ) );
 
-    interface.spiRead = FakeRead_Read32;
+    interface.spiRead = FakeRead_Read8Arr;
     interface.spiSetCS = WriteSpy_Write8;
 
     TEST_ASSERT_EQUAL( AS5047P_INTERFACE_SET, AS5047P_SetInterface( &interface ) );
 }
 
+static uint32_t concatenate32( uint8_t* arr )
+{
+    return ( uint32_t )( arr[ 0 ] << 24 ) | ( arr[ 1 ] << 16 ) | ( arr[ 2 ] << 8 ) | arr[ 3 ];
+}
+
 TEST( AS5047P, WriteBit30Low )
 {
     AS5047P_Write( 0xFFFFFFFF, ZPOSL );
-    TEST_ASSERT_BIT_LOW( 30, WriteSpy_GetLastWrite32() );
+    TEST_ASSERT_BIT_LOW( 30, concatenate32( WriteSpy_GetLastWrite8Arr() ) );
 }
 
 TEST( AS5047P, ReadBit30High )
 {
     AS5047P_Read( NOP );
-    TEST_ASSERT_BIT_HIGH( 30, FakeRead_GetLastCmd32() );
+    TEST_ASSERT_BIT_HIGH( 30, concatenate32( FakeRead_GetLastCmd8Arr() ) );
 }
 
 TEST( AS5047P, PreventWriteToReadOnlyAddresses )
 {
     AS5047P_Write( 0xFFFFFFFF, NOP );
-    TEST_ASSERT_EQUAL( 0, WriteSpy_GetLastWrite32() );
+    TEST_ASSERT_NULL( WriteSpy_GetLastWrite8Arr() );
 
     AS5047P_Write( 0xFFFFFFFF, DIAAGC );
-    TEST_ASSERT_EQUAL( 0, WriteSpy_GetLastWrite32() );
+    TEST_ASSERT_NULL( WriteSpy_GetLastWrite8Arr() );
 }
 
 TEST( AS5047P, WriteCmdOutParityAndEFBitsLow )
 {
     AS5047P_Write( 0xFFFFFFFF, PROG );
-    TEST_ASSERT_BIT_LOW( 31, WriteSpy_GetLastWrite32() );
-    TEST_ASSERT_BIT_LOW( 15, WriteSpy_GetLastWrite32() );
-    TEST_ASSERT_BIT_LOW( 14, WriteSpy_GetLastWrite32() );
+    uint32_t lastWrite = concatenate32( WriteSpy_GetLastWrite8Arr() );
+    TEST_ASSERT_BIT_LOW( 31, lastWrite );
+    TEST_ASSERT_BIT_LOW( 15, lastWrite );
+    TEST_ASSERT_BIT_LOW( 14, lastWrite );
 }
 
 TEST( AS5047P, ReadCmdOutParityAndEFBitsLow )
 {
     AS5047P_Read( PROG );
-    TEST_ASSERT_BIT_LOW( 31, FakeRead_GetLastCmd32() );
-    TEST_ASSERT_BIT_LOW( 15, FakeRead_GetLastCmd32() );
-    TEST_ASSERT_BIT_LOW( 14, FakeRead_GetLastCmd32() );
+    uint32_t lastCmd = concatenate32( FakeRead_GetLastCmd8Arr() );
+    TEST_ASSERT_BIT_LOW( 31, lastCmd );
+    TEST_ASSERT_BIT_LOW( 15, lastCmd );
+    TEST_ASSERT_BIT_LOW( 14, lastCmd );
 }
 
 TEST( AS5047P, ReadPlaceAddressBitsInBits29Through16 )
 {
     AS5047P_Read( PROG );
-    TEST_ASSERT_BITS( 0x3FFF0000, 0x00030000, FakeRead_GetLastCmd32() );
+    TEST_ASSERT_BITS( 0x3FFF0000, 0x00030000, concatenate32( FakeRead_GetLastCmd8Arr() ) );
 }
 
 TEST( AS5047P, WritePlaceAddressBitsInBits29Through16 )
 {
     AS5047P_Write( 0xFFFFFFFF, PROG );
-    TEST_ASSERT_BITS( 0x3FFF0000, 0x00030000, WriteSpy_GetLastWrite32() );
+    TEST_ASSERT_BITS( 0x3FFF0000, 0x00030000, concatenate32( WriteSpy_GetLastWrite8Arr() ) );
 }
 
 TEST( AS5047P, RestrictDataBitsToBits13Through0 )
 {
     AS5047P_Write( 0xFFFFFFFF, PROG );
-    TEST_ASSERT_EQUAL_HEX32( 0x00033FFF, WriteSpy_GetLastWrite32() );
+    TEST_ASSERT_EQUAL_HEX32( 0x00033FFF, concatenate32( WriteSpy_GetLastWrite8Arr() ) );
 }
 
 TEST( AS5047P, ReadDataBitsCleared )
 {
     AS5047P_Read( ZPOSM );
-    TEST_ASSERT_BITS_LOW( 0x00003FFF, FakeRead_GetLastCmd32() );
+    TEST_ASSERT_BITS_LOW( 0x00003FFF, concatenate32( FakeRead_GetLastCmd8Arr() ) );
 }
 
 TEST( AS5047P, PreventReadFromOutOfBoundsAddress )
 {
     TEST_ASSERT_EQUAL( AS5047P_OUT_OF_BOUNDS_ADDRESS, AS5047P_Read( DIAAGC - 1 ) );
-    TEST_ASSERT_EQUAL( 0, FakeRead_GetLastCmd32() );
+    TEST_ASSERT_NULL( FakeRead_GetLastCmd8Arr() );
 
     TEST_ASSERT_EQUAL( AS5047P_OUT_OF_BOUNDS_ADDRESS, AS5047P_Read( ZPOSM  - 1 ) );
-    TEST_ASSERT_EQUAL( 0, FakeRead_GetLastCmd32() );
+    TEST_ASSERT_NULL( FakeRead_GetLastCmd8Arr() );
 
     TEST_ASSERT_EQUAL( AS5047P_OUT_OF_BOUNDS_ADDRESS, AS5047P_Read( ERRFL  + 1 ) );
-    TEST_ASSERT_EQUAL( 0, FakeRead_GetLastCmd32() );
+    TEST_ASSERT_NULL( FakeRead_GetLastCmd8Arr() );
 }
 
 TEST( AS5047P, ReadEnsuresParityBitsBackAreSet )
 {
-    FakeRead_SetNextReading32( 0x000000FF );
+    nextReading[ 0 ] = 0x00;
+    nextReading[ 1 ] = 0x00;
+    nextReading[ 2 ] = 0x00;
+    nextReading[ 3 ] = 0xFF;
+    FakeRead_SetNextReading8Arr( nextReading, sizeof( nextReading ) );
     TEST_ASSERT_EQUAL( AS5047P_PARITY_FAIL, AS5047P_Read( PROG ) );
 
-    FakeRead_SetNextReading32( 0x000080FF );
+    nextReading[ 0 ] = 0x00;
+    nextReading[ 1 ] = 0x00;
+    nextReading[ 2 ] = 0x80;
+    nextReading[ 3 ] = 0xFF;
+    FakeRead_SetNextReading8Arr( nextReading, sizeof( nextReading ) );
     TEST_ASSERT_EQUAL( AS5047P_PARITY_FAIL, AS5047P_Read( PROG ) );
 
-    FakeRead_SetNextReading32( 0x800000FF );
+    nextReading[ 0 ] = 0x80;
+    nextReading[ 1 ] = 0x00;
+    nextReading[ 2 ] = 0x00;
+    nextReading[ 3 ] = 0xFF;
+    FakeRead_SetNextReading8Arr( nextReading, sizeof( nextReading ) );
     TEST_ASSERT_EQUAL( AS5047P_PARITY_FAIL, AS5047P_Read( PROG ) );
 }
 
 TEST( AS5047P, ReadEnsuresEFBackIsLow )
 {
-    FakeRead_SetNextReading32( 0x8000C0FF );
+    nextReading[ 0 ] = 0x80;
+    nextReading[ 1 ] = 0x00;
+    nextReading[ 2 ] = 0xC0;
+    nextReading[ 3 ] = 0xFF;
+    FakeRead_SetNextReading8Arr( nextReading, sizeof( nextReading ) );
     TEST_ASSERT_EQUAL( AS5047P_EF_FAIL, AS5047P_Read( PROG ) );
 }
 
@@ -146,7 +175,7 @@ TEST( AS5047P, SetPROGMemberBitsPlacedCorrectly )
         .progen  = 1
     };
     AS5047P_SetPROG( &cfg );
-    TEST_ASSERT_BITS( 0x00003FFF, 0x00000049, WriteSpy_GetLastWrite32() );
+    TEST_ASSERT_BITS( 0x00003FFF, 0x00000049, concatenate32( WriteSpy_GetLastWrite8Arr() ) );
 }
 
 TEST( AS5047P, SetZPOSMMemberBitsPlacedCorrectly )
@@ -156,7 +185,7 @@ TEST( AS5047P, SetZPOSMMemberBitsPlacedCorrectly )
         .zposMSB = 15
     };
     AS5047P_SetZPOSM( &cfg );
-    TEST_ASSERT_BITS( 0x00003FFF, 0x0000000F, WriteSpy_GetLastWrite32() );
+    TEST_ASSERT_BITS( 0x00003FFF, 0x0000000F, concatenate32( WriteSpy_GetLastWrite8Arr() ) );
 }
 
 TEST( AS5047P, SetZPOSLMemberBitsPlacedCorrectly )
@@ -168,7 +197,7 @@ TEST( AS5047P, SetZPOSLMemberBitsPlacedCorrectly )
         .comp_h_error_en = 0
     };
     AS5047P_SetZPOSL( &cfg );
-    TEST_ASSERT_BITS( 0x00003FFF, 0x0000051, WriteSpy_GetLastWrite32() );
+    TEST_ASSERT_BITS( 0x00003FFF, 0x0000051, concatenate32( WriteSpy_GetLastWrite8Arr() ) );
 }
 
 TEST( AS5047P, SetSETTINGS1MemberBitsPlacedCorrectly )
@@ -183,7 +212,7 @@ TEST( AS5047P, SetSETTINGS1MemberBitsPlacedCorrectly )
         .pwmOn           = 0
     };
     AS5047P_SetSETTINGS1( &cfg );
-    TEST_ASSERT_BITS( 0x00003FFF, 0x00000054, WriteSpy_GetLastWrite32() );
+    TEST_ASSERT_BITS( 0x00003FFF, 0x00000054, concatenate32( WriteSpy_GetLastWrite8Arr() ) );
 }
 
 TEST( AS5047P, SetSETTINGS2MemberBitsPlacedCorrectly )
@@ -195,7 +224,7 @@ TEST( AS5047P, SetSETTINGS2MemberBitsPlacedCorrectly )
         .abires = 5
     };
     AS5047P_SetSETTINGS2( &cfg );
-    TEST_ASSERT_BITS( 0x00003FFF, 0x000000B6, WriteSpy_GetLastWrite32() );
+    TEST_ASSERT_BITS( 0x00003FFF, 0x000000B6, concatenate32( WriteSpy_GetLastWrite8Arr() ) );
 }
 
 TEST( AS5047P, SetPROGFailIfMembersBitsExceedExpected )
@@ -208,12 +237,12 @@ TEST( AS5047P, SetPROGFailIfMembersBitsExceedExpected )
         .progen  = 100 
     };
     AS5047P_SetPROG( &cfg );
-    TEST_ASSERT_EQUAL( 0, WriteSpy_GetLastWrite32() );
+    TEST_ASSERT_NULL( WriteSpy_GetLastWrite8Arr() );
 }
 
 // Not needed since zposm is 8 bits and the member is uint8_t
 // so it will not exceed
-//TEST( AS5047P, SetZPOSMFailIfMembersBitsExceedExpected )
+//IGNORE_TEST( AS5047P, SetZPOSMFailIfMembersBitsExceedExpected )
 //{
 //}
 
@@ -226,7 +255,7 @@ TEST( AS5047P, SetZPOSLFullFrameCheck )
         .comp_h_error_en = 0
     };
     AS5047P_SetZPOSL( &cfg );
-    TEST_ASSERT_EQUAL_HEX32( 0x00170051, WriteSpy_GetLastWrite32() );
+    TEST_ASSERT_EQUAL_HEX32( 0x00170051, concatenate32( WriteSpy_GetLastWrite8Arr() ) );
 }
 
 
@@ -239,7 +268,7 @@ TEST( AS5047P, SetZPOSLFailIfMembersBitsExceedExpected )
         .comp_h_error_en = 2 
     };
     AS5047P_SetZPOSL( &cfg );
-    TEST_ASSERT_EQUAL( 0, WriteSpy_GetLastWrite32() );
+    TEST_ASSERT_NULL( WriteSpy_GetLastWrite8Arr() );
 }
 
 TEST( AS5047P, SetSETTINGS1FailIfMembersBitsExceedExpected )
@@ -254,7 +283,7 @@ TEST( AS5047P, SetSETTINGS1FailIfMembersBitsExceedExpected )
         .pwmOn           = 0
     };
     AS5047P_SetSETTINGS1( &cfg );
-    TEST_ASSERT_EQUAL( 0, WriteSpy_GetLastWrite32() );
+    TEST_ASSERT_NULL( WriteSpy_GetLastWrite8Arr() );
 }
 
 TEST( AS5047P, SetSETTINGS2FailIfMembersBitsExceedExpected )
@@ -266,35 +295,39 @@ TEST( AS5047P, SetSETTINGS2FailIfMembersBitsExceedExpected )
         .abires = 8
     };
     AS5047P_SetSETTINGS2( &cfg );
-    TEST_ASSERT_EQUAL( 0, WriteSpy_GetLastWrite32() );
+    TEST_ASSERT_NULL( WriteSpy_GetLastWrite8Arr() );
 }
 
 // if above is implemented then the following shouldn't be necessary
 // since not possible for bits to bleed into surrounding bit areas
 //
-//TEST( AS5047P, SetPROGNonSettableBitsLow )
+//IGNORE_TEST( AS5047P, SetPROGNonSettableBitsLow )
 //{
 //}
 //
-//TEST( AS5047P, SetZPOSMNonSettableBitsLow )
+//IGNORE_TEST( AS5047P, SetZPOSMNonSettableBitsLow )
 //{
 //}
 //
-//TEST( AS5047P, SetZPOSLNonSettableBitsLow )
+//IGNORE_TEST( AS5047P, SetZPOSLNonSettableBitsLow )
 //{
 //}
 //
-//TEST( AS5047P, SetSETTINGS1NonSettableBitsLow )
+//IGNORE_TEST( AS5047P, SetSETTINGS1NonSettableBitsLow )
 //{
 //}
 //
-//TEST( AS5047P, SetSETTINGS2NonSettableBitsLow )
+//IGNORE_TEST( AS5047P, SetSETTINGS2NonSettableBitsLow )
 //{
 //}
 
 TEST( AS5047P, GetERRFLMemberBitsExtractedCorrectly )
 {
-    FakeRead_SetNextReading32( 0x8000815A );
+    nextReading[ 0 ] = 0x80;
+    nextReading[ 1 ] = 0x00;
+    nextReading[ 2 ] = 0x81;
+    nextReading[ 3 ] = 0x5A;
+    FakeRead_SetNextReading8Arr( nextReading, sizeof( nextReading ) );
 
     AS5047PERRFL errfl = { 0 };
     AS5047P_GetERRFL( &errfl );
@@ -306,7 +339,11 @@ TEST( AS5047P, GetERRFLMemberBitsExtractedCorrectly )
 
 TEST( AS5047P, GetPROGMemberBitsExtractedCorrectly )
 {
-    FakeRead_SetNextReading32( 0x8000815A );
+    nextReading[ 0 ] = 0x80;
+    nextReading[ 1 ] = 0x00;
+    nextReading[ 2 ] = 0x81;
+    nextReading[ 3 ] = 0x5A;
+    FakeRead_SetNextReading8Arr( nextReading, sizeof( nextReading ) );
 
     AS5047PPROG prog = { 0 };
     AS5047P_GetPROG( &prog );
@@ -319,7 +356,11 @@ TEST( AS5047P, GetPROGMemberBitsExtractedCorrectly )
 
 TEST( AS5047P, GetDIAAGCMemberBitsExtractedCorrectly )
 {
-    FakeRead_SetNextReading32( 0x8000815A );
+    nextReading[ 0 ] = 0x80;
+    nextReading[ 1 ] = 0x00;
+    nextReading[ 2 ] = 0x81;
+    nextReading[ 3 ] = 0x5A;
+    FakeRead_SetNextReading8Arr( nextReading, sizeof( nextReading ) );
 
     AS5047PDIAAGC diaagc = { 0 };
     AS5047P_GetDIAAGC( &diaagc );
@@ -333,7 +374,11 @@ TEST( AS5047P, GetDIAAGCMemberBitsExtractedCorrectly )
 
 TEST( AS5047P, GetMAGMemberBitsExtractedCorrectly )
 {
-    FakeRead_SetNextReading32( 0x8000815A );
+    nextReading[ 0 ] = 0x80;
+    nextReading[ 1 ] = 0x00;
+    nextReading[ 2 ] = 0x81;
+    nextReading[ 3 ] = 0x5A;
+    FakeRead_SetNextReading8Arr( nextReading, sizeof( nextReading ) );
     
     AS5047PMAG mag = { 0 };
     AS5047P_GetMAG( &mag );
@@ -343,7 +388,11 @@ TEST( AS5047P, GetMAGMemberBitsExtractedCorrectly )
 
 TEST( AS5047P, GetANGLEUNCMemberBitsExtractedCorrectly )
 {
-    FakeRead_SetNextReading32( 0x8000815A );
+    nextReading[ 0 ] = 0x80;
+    nextReading[ 1 ] = 0x00;
+    nextReading[ 2 ] = 0x81;
+    nextReading[ 3 ] = 0x5A;
+    FakeRead_SetNextReading8Arr( nextReading, sizeof( nextReading ) );
 
     AS5047PANGLEUNC angleunc = { 0 };
     AS5047P_GetANGLEUNC( &angleunc );
@@ -353,29 +402,39 @@ TEST( AS5047P, GetANGLEUNCMemberBitsExtractedCorrectly )
 
 TEST( AS5047P, GetANGLECOMMemberBitsExtractedCorrectly )
 {
-    FakeRead_SetNextReading32( 0x8000815A );
+    nextReading[ 0 ] = 0x80;
+    nextReading[ 1 ] = 0x00;
+    nextReading[ 2 ] = 0x81;
+    nextReading[ 3 ] = 0x5A;
+    FakeRead_SetNextReading8Arr( nextReading, sizeof( nextReading ) );
 
     AS5047PANGLECOM anglecom = { 0 };
     AS5047P_GetANGLECOM( &anglecom );
 
     TEST_ASSERT_EQUAL_HEX16( 0x015A , anglecom.anglecom );
-
 }
 
 TEST( AS5047P, GetZPOSMMemberBitsExtractedCorrectly )
 {
-    FakeRead_SetNextReading32( 0x8000815A );
+    nextReading[ 0 ] = 0x80;
+    nextReading[ 1 ] = 0x00;
+    nextReading[ 2 ] = 0x81;
+    nextReading[ 3 ] = 0x5A;
+    FakeRead_SetNextReading8Arr( nextReading, sizeof( nextReading ) );
 
     AS5047PZPOSM zposm = { 0 };
     AS5047P_GetZPOSM( &zposm );
 
     TEST_ASSERT_EQUAL_HEX8( 0x5A, zposm.zposMSB );
-
 }
 
 TEST( AS5047P, GetZPOSLMemberBitsExtractedCorrectly )
 {
-    FakeRead_SetNextReading32( 0x8000815A );
+    nextReading[ 0 ] = 0x80;
+    nextReading[ 1 ] = 0x00;
+    nextReading[ 2 ] = 0x81;
+    nextReading[ 3 ] = 0x5A;
+    FakeRead_SetNextReading8Arr( nextReading, sizeof( nextReading ) );
 
     AS5047PZPOSL zposl = { 0 };
     AS5047P_GetZPOSL( &zposl );
@@ -387,7 +446,11 @@ TEST( AS5047P, GetZPOSLMemberBitsExtractedCorrectly )
 
 TEST( AS5047P, GetSETTINGS1MemberBitsExtractedCorrectly )
 {
-    FakeRead_SetNextReading32( 0x8000815A );
+    nextReading[ 0 ] = 0x80;
+    nextReading[ 1 ] = 0x00;
+    nextReading[ 2 ] = 0x81;
+    nextReading[ 3 ] = 0x5A;
+    FakeRead_SetNextReading8Arr( nextReading, sizeof( nextReading ) );
 
     AS5047PSETTINGS1 settings1 = { 0 };
     AS5047P_GetSETTINGS1( &settings1 );
@@ -404,7 +467,11 @@ TEST( AS5047P, GetSETTINGS1MemberBitsExtractedCorrectly )
 
 TEST( AS5047P, GetSETTINGS2MemberBitsExtractedCorrectly )
 {
-    FakeRead_SetNextReading32( 0x8000815A );
+    nextReading[ 0 ] = 0x80;
+    nextReading[ 1 ] = 0x00;
+    nextReading[ 2 ] = 0x81;
+    nextReading[ 3 ] = 0x5A;
+    FakeRead_SetNextReading8Arr( nextReading, sizeof( nextReading ) );
 
     AS5047PSETTINGS2 settings2 = { 0 };
     AS5047P_GetSETTINGS2( &settings2 );

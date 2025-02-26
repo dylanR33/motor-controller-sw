ff67378f1320a6401b6151de6bdc6d342a9da586
dylanR33 <ruiz.dylaneduardo@gmail.com>
2025-02-26 14:14:34 -0800

    Added documentation to interface of gate driver firmware.

--- src/drivers/DRV8323/DRV8323.h
+++ src/drivers/DRV8323/DRV8323.h
@@ -11,12 +11,13 @@ typedef enum
 } DRV8323Status;
 
 
-enum
+typedef enum
 {
     OUT_OF_BOUNDS_ADDRESS
-};
+} DRV8323Failures;
 
 
+// Register address definitions
 typedef enum
 {
     FAULT_STATUS1,
@@ -29,6 +30,7 @@ typedef enum
 } DRV8323_Register;
 
 
+// State of enable pin
 typedef enum
 {
     ENABLE_OFF,
@@ -36,6 +38,28 @@ typedef enum
 } DRV8323_EnableState;
 
 
+// Interface defining callbacks for SPI communication, ADC sampling, and GPIO logic levels
+//
+// spiWrite  : SPI transmission callback
+//      data : buffer containing transmision data
+//      size : size in bytes of transmission data buffer
+//
+// spiRead     : SPI transmission-receival callback
+//      cmd    : buffer containing transmission data
+//      rxBuff : buffer to store read back data
+//      size   : size of both buffers (should be equal)
+//
+// spiSetCS   : SPI chip select gpio logic state setting callback
+//      state : chip select gpio state (1 = high, 0 = low)
+//
+// adcReadRawPhaseA : ADC sampling callback
+//
+// adcReadRawPhaseB : ADC sampling callback
+//
+// adcReadRawPhaseC : ADC sampling callback
+//
+// setEnablePin : enable pin GPIO logic state setting callback
+//      state : gpio state (1 = high, 0 = low)
 typedef struct
 {
     void     ( *spiWrite         ) ( uint8_t* data, uint16_t size );
@@ -47,6 +71,11 @@ typedef struct
     void     ( *setEnablePin     ) ( uint8_t state );
 } DRV8323Interface;
 
+
+// The following unions define the data significance contained within each register.
+// The anonymous structures within define the bit layout of the structure through the
+// use of bit fields. A raw value is provided within the union for easy access of the
+// full register word.
 
 typedef union
 {
@@ -170,6 +199,16 @@ typedef union
 } DRV8323csaCtrl;
 
 
+// Structure used for calculation of phase current from ADC
+// sample voltage.
+//
+// vref : ADC reference voltage
+//
+// adcSteps : max integer value producable by ADC (2^resolution)
+//
+// rSense : ohms sense resistor
+//
+// csaGain : current sense amplifier gain
 typedef struct
 {
     float    vRef;
@@ -179,42 +218,130 @@ typedef struct
 } DRV8323CurrentSenseCfg;
 
 
+// Install hardware callbacks into the driver. All callbacks must
+// be defined to be successful.
+//
+// inter : structure defining each callback function
+//
+// returns : status of interface setting
 DRV8323Status DRV8323_SetInterface( DRV8323Interface* inter );
 
+
+// Write data value into register.
+//
+// data : data value to write into register address
+//
+// address : address of register to write to
 void DRV8323_Write( uint16_t data, uint8_t address );
 
+
+// Read data of register.
+//
+// address : address of register to read from
+//
+// return : read back data
 uint16_t DRV8323_Read( uint8_t address );
 
+
+// Configure driver control register.
+//
+// config : union defining values of each field within the register
 void DRV8323_SetDriverCtrl( DRV8323driverCtrl* config );
 
+
+// Configure gate drive high side register.
+//
+// config : union defining values of each field within the register
 void DRV8323_SetGateDriveHS( DRV8323gateDriveHS* config );
 
+
+// Configure gate drive low side register.
+//
+// config : union defining values of each field within the register
 void DRV8323_SetGateDriveLS( DRV8323gateDriveLS* config );
 
+
+// Configure overcurrent protection register.
+//
+// config : union defining values of each field within the register
 void DRV8323_SetOCPCtrl( DRV8323ocpCtrl* config );
 
+
+// Configure current sense amplifier register.
+//
+// config : union defining values of each field within the register
 void DRV8323_SetCSACtrl( DRV8323csaCtrl* config );
 
+
+// Retreive fault status 1 register.
+//
+// fstat1 : union to hold each field within the register
 void DRV8323_GetFaultStatus1( DRV8323faultStatus1* fstat1 );
 
+
+// Retreive fault status 2 register.
+//
+// fstat2 : union to hold each field within the register
 void DRV8323_GetFaultStatus2( DRV8323faultStatus2* fstat2 );
 
+
+// Retreive driver control register.
+//
+// driverCtrl : union to hold each field within the register
 void DRV8323_GetDriverCtrl( DRV8323driverCtrl* driverCtrl );
 
+
+// Retreive gate drive high side register.
+//
+// gateDriveHS : union to hold each field within the register
 void DRV8323_GetGateDriveHS( DRV8323gateDriveHS* gateDriveHS );
 
+
+// Retreive gate drive low side register.
+//
+// gateDriveLS : union to hold each field within the register
 void DRV8323_GetGateDriveLS( DRV8323gateDriveLS* gateDriveLS );
 
+
+// Retreive over current protection register.
+//
+// ocpCtrl : union to hold each field within the register
 void DRV8323_GetOCPCtrl( DRV8323ocpCtrl* ocpCtrl );
 
+
+// Retreive current sense amplifier register.
+//
+// csaCtrl : union to hold each field within the register
 void DRV8323_GetCSACtrl( DRV8323csaCtrl* csaCtrl );
 
+
+// Retrive phase current of phase A
+//
+// iCfg : structure defining values used in calculation of phase current
+//
+// return : calculated phase current
 float DRV8323_GetPhaseCurrentA( DRV8323CurrentSenseCfg* iCfg );
 
+
+// Retrive phase current of phase B
+//
+// iCfg : structure defining values used in calculation of phase current
+//
+// return : calculated phase current
 float DRV8323_GetPhaseCurrentB( DRV8323CurrentSenseCfg* iCfg );
 
+
+// Retrive phase current of phase C
+//
+// iCfg : structure defining values used in calculation of phase current
+//
+// return : calculated phase current
 float DRV8323_GetPhaseCurrentC( DRV8323CurrentSenseCfg* iCfg );
 
+
+// Set the GPIO logic state of the enable pin
+//
+// state : enable logic state (1 = high, 0 = low)
 void DRV8323_SetEnableState( DRV8323_EnableState state );
 
 #endif
